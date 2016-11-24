f75643d8635d1788305db2a74860f65b3f247e2f
Mariana <mari.anasieveg2@gmail.com>
2016-11-23 19:41:25 -0600

    Movimientos de carpetas

---
+++ Clases 0.2/Shift.java
@@ -0,0 +1,10 @@
+package com.game.shift;
+
+import com.game.shift.graficos.Background;
+
+public class Shift {
+	public static void main(String[] args){
+	Background game = new Background();
+	game.start();
+	}
+}
