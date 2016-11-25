ce4bc555b3f4f7a86c1467f41f3e926c48950dbf
Mariana <mari.anasieveg2@gmail.com>
2016-11-25 10:03:41 -0600

    Bordes FUNCIONANDO
    El rebote con los jugadores con los bordes funcionan

--- src/com/game/shift/entity/mob/PlayerOne.java
+++ src/com/game/shift/entity/mob/PlayerOne.java
@@ -21,26 +21,7 @@ public class PlayerOne extends Player {
 		
 		if(xa!= 0 || ya!= 0) move (xa, ya, screen); 
 	}
-	
-	public void move(int xa, int ya, Screen screen){
-		if(xa > 0) dir = 1;
-		if(xa < 0) dir = 3;
-		if(ya > 0) dir = 2;
-		if(ya < 0) dir = 0;
 		
-		if ( dir == 1 && collisionBarra(screen)) xa--;
-		if( dir == 3 && collisionBarra(screen)) xa++;
-		if( dir == 2 && collisionBarra(screen)) ya--;
-		
-		if( x + xa >= Background.width_p-8) xa--;
-		if( x + xa <= 0) xa++;
-		if( y + ya >= Background.height_p-8) ya--;
-		if( y + ya <= 10) ya++;
-		
-		x += xa;
-		y += ya;	
-	}
-	
 	private void posRandom(){
 		x = Background.width_p/4;
 		y = Background.height_p/2;
